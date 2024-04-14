fa1e19534ccd5d51e48276668642dcbd933d56e7
rick010 <ricardo.dev.com@gmail.com>
2024-04-14 13:55:47 -0300

    arquivo .sql created na path raiz

---
+++ create.sql
@@ -0,0 +1,10 @@
+create table tb_product (price float(53), created_at TIMESTAMP WITHOUT TIME ZONE, id bigserial not null, updated_at TIMESTAMP WITHOUT TIME ZONE, category varchar(255), img varchar(255), name varchar(255), primary key (id));
+create table tb_user (role smallint check (role between 0 and 1), id bigserial not null, email varchar(255) unique, name varchar(255), password varchar(255), primary key (id));
+INSERT INTO tb_user (name, email, password) VALUES ('John', 'John@mail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Admin', 'admin@mail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_product(name, category, price, img, created_At) VALUES ('Hamburguer', 'Sanduíches', 14, 'https://i.imgur.com/Vng6VzV.png', NOW());
+INSERT INTO tb_product(name, category, price, img, created_At) VALUES ('X-Burguer', 'Sanduíches', 16, 'https://i.imgur.com/soOUeeW.png', NOW());
+INSERT INTO tb_product(name, category, price, img, created_At) VALUES ('Big Kenzie', 'Sanduíches', 18, 'https://i.imgur.com/eEzZzcF.png', NOW());
+INSERT INTO tb_product(name, category, price, img, created_At) VALUES ('Fanta Guaraná', 'Bebidas', 5, 'https://i.imgur.com/YuIbfCi.png', NOW());
+INSERT INTO tb_product(name, category, price, img, created_At) VALUES ('Coca-Cola', 'Bebidas', 4.99, 'https://i.imgur.com/KC2ihEN.png', NOW());
+INSERT INTO tb_product(name, category, price, img, created_At) VALUES ('Milkshake Ovomaltine', 'Bebidas', 4.99, 'https://i.imgur.com/iNkD4Pq.png', NOW());
